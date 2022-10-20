1054c289e6551db373ec2245d33ff7abfac15a73
Michelle Shelby <64713087+ivoryspren@users.noreply.github.com>
2022-10-19 20:39:10 -0500

    Added code per homework instructions

--- Homework3.sol
+++ Homework3.sol
@@ -11,16 +11,26 @@ deployer
 
 pragma solidity 0.8.17;
 
-
 contract BootcampContract {
 
     uint256 number;
+    address owner;
 
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function getOwnerAddress() external view returns (address) {
+        if (msg.sender == owner) {
+            return 0x000000000000000000000000000000000000dEaD;
+        } else {
+            return owner;
+        }    
+    }
 
     function store(uint256 num) public {
         number = num;
     }
-
 
     function retrieve() public view returns (uint256){
         return number;
