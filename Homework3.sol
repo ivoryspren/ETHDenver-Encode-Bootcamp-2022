218dd5d60101792660c254a94297db1996993d8e
Michelle Shelby <64713087+ivoryspren@users.noreply.github.com>
2022-10-19 19:49:09 -0500

    Adding homework instructions in comment

--- Homework3.sol
+++ Homework3.sol
@@ -1,3 +1,12 @@
+/* Homework 3
+To this contract
+. Add a variable to hold the address of the deployer of the contract
+. Update that variable with the deployer's address when the contract is deployed.
+. Write an external function to return
+. Address 0x000000000000000000000000000000000000dEaD if called by the
+deployer
+. The deployer's address otherwise */
+
 // SPDX-License-Identifier: None
 
 pragma solidity 0.8.17;
